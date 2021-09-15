8288a53e30ffbf89469d9a80a36710b41b2ba582
EthanPan6 <alwaysmrp@vip.qq.com>
2021-09-15 16:41:21 +0800

    delete vite

--- demov3/src/App.vue
+++ demov3/src/App.vue
@@ -1,27 +1,28 @@
 <template>
-  <!-- <Counter />
+  <Counter />
   <Cpn1 msg="大家好" time="2021/09/14" @hello="test">
     <p>大家好(来自插槽的信息)</p>
-  </Cpn1> -->
+  </Cpn1>
   <!-- <LifeCycle v-if="isShow" key="LifeCycle" />
   <button @click="changeState">{{ isShow ? "卸载" : "挂载" }}</button> -->
-  <GetPoint />
+  <!-- <GetPoint /> -->
 </template>
 
 <script>
 // import { ref, reactive } from "vue";
-// import Cpn1 from "./components/Cpn1.vue";
+import Cpn1 from "./components/Cpn1.vue";
 // import Counter from "./components/Counter.vue";
 // import LifeCycle from "./components/LifeCycles.vue";
-import GetPoint from "./components/GetPoint.vue";
+// import GetPoint from "./components/GetPoint.vue";
 export default {
   name: "App",
   data() {
     return { isShow: true };
   },
   components: {
-    //  Cpn1, Counter,
-    GetPoint,
+    Cpn1,
+    // Counter,
+    // GetPoint,
   },
   methods: {
     changeState() {
