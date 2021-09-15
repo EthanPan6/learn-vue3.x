01085a0134e2bac07ff806fd52ba778e3b744007
EthanPan6 <alwaysmrp@vip.qq.com>
2021-09-15 17:48:08 +0800

    ts

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+import  Demo from './components/Demo.vue'
+import {ref,onMounted}from 'vue'
+function getMsg(msg:String):void{
+console.log(msg);
+}
+const sayHi=ref('大家好')
+const propsEmitRef=ref()
+
+onMounted(()=>{
+console.log(propsEmitRef.value.child);
+
+propsEmitRef.value.childNode()
+})
+</script>
+
+<template>
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <Demo @hello="getMsg" :msg='sayHi' ref="propsEmitRef"/>
+</template>
+
+<style>
+</style>
---
+++ my-vue-app/src/components/Setup语法糖.vue
@@ -0,0 +1,31 @@
+
+<!-- <script setup lang="ts">
+const props=defineProps({
+  title:{
+    type:String,
+    default:()=>'测试内容'
+  }
+})
+console.log(props.title);
+alert('888')
+</script> -->
+<script  lang="ts">
+export default{
+  props:{
+    title:{
+      type:String,
+      default:()=>'测试信息'
+    }
+  },
+  setup(props:any){
+    console.log(props.title);
+    
+  }
+}
+</script>
+<template>
+<h2>大家好</h2>
+</template>
+
+<style>
+</style>
